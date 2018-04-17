df27b4498083a3485a1dd93a72d5323df9dc8475
zhang liang <zhangliang@usechain.net>
2018-04-17 11:14:55 +0800

    fix db path

--- sqlite.py
+++ sqlite.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/lyszhang/PycharmProjects/ipfs-video/testDB.db '
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testDB.db '
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
